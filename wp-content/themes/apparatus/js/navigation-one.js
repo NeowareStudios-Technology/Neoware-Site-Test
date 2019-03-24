3d2d00c1557d4253357493b1deed55f369ab66d9
Alfonso Morales <Alfonso@leapwithalice.io>
2019-03-24 14:40:17 -0400

    Update to add studios page

---
+++ wp-content/themes/apparatus/js/navigation-one.js
@@ -0,0 +1,1 @@
+(function($){"use strict";jQuery(window).on("scroll",function(){var scroll=jQuery(window).scrollTop();if(scroll>=60){jQuery(".navbar").addClass("bg-scroll");}else{jQuery(".navbar").removeClass("bg-scroll");}});})(jQuery);
